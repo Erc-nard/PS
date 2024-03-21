07ea99d0123a3c7163f0d1bf505ecce5b56d48bc
Ercnard <100018037+Erc-nard@users.noreply.github.com>
2024-03-21 16:41:28 +0900

    Delete 소수구하기.py

--- 소수구하기.py
+++
@@ -1,6 +0,0 @@
-m,n=map(int,input().split())
-for i in range(m,n+1):
-    if i%2!=0 and i%3!=0 and i%5!=0 and i%7!=0:
-        print(i)
-    if i==1 or i==2 or i==3 or i==5 or i==7:
-        print(i)
