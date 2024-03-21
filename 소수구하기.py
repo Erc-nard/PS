8b38549371b6d20033c92513e491b9962800448a
Erc-nard <100018037+Erc-nard@users.noreply.github.com>
2024-03-21 17:29:23 +0900

    time out

--- 소수구하기.py
+++ 소수구하기.py
@@ -1,6 +1,23 @@
-m,n=map(int,input().split())
-for i in range(m,n+1):
-    if i%2!=0 and i%3!=0 and i%5!=0 and i%7!=0:
-        print(i)
-    if i==1 or i==2 or i==3 or i==5 or i==7:
-        print(i)
+n,m=map(int,input().split())
+prime=[] #이전 소수들로 나눠지는지 확인하면됨
+prime.append(2)
+num=0
+for i in range(3,m):
+    if(i%2!=0):
+        check=0
+        for j in range(num):
+            if((int)(i%prime[j])==0):
+                check=1
+                break
+        if check==0:
+            prime.append(i)
+            num+=1
+
+for i in range(len(prime)):
+    if(prime[i]<n):
+        continue
+    if(prime[i]>m):
+        break
+    print(prime[i])
+
+
