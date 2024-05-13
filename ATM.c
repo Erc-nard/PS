c292787800344eb0584ea135357d1cf69c89916a
Ercnard <100018037+Erc-nard@users.noreply.github.com>
2024-05-13 11:04:09 +0900

    Create ATM.c

---
+++ ATM.c
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+void merge(int* A, int p, int q, int r) {
+	int i = p, j = q+1, now = 0;
+	int* bucket = (int*)malloc(sizeof(int) * r);
+
+	while (i <= q && j <= r) {
+		if (A[i] > A[j])
+			bucket[now++] = A[j++];
+		else bucket[now++] = A[i++];
+	}
+	
+	while (i <= q) {
+		bucket[now++] = A[i++];
+	}
+	while (j < r) {
+		bucket[now++] = A[j++];
+	}
+
+	for (int k = 0; k < r; k++) {
+		A[k] = bucket[k];
+	}
+	
+}
+void mergeSort(int* A, int p, int r) {
+
+	if (p < r) {
+		int q = (p + r) / 2;
+		mergeSort(A, p, q);
+		mergeSort(A, q + 1, r);
+		merge(A, p, q, r);
+	}
+}
+
+int main(){
+	int n;
+	
+	scanf("%d", &n);
+
+	int* a = (int*)malloc(sizeof(int) * n);
+
+	for (int i = 0; i < n; i++)
+		scanf("%d", &a[i]);
+
+	mergeSort(a, 0, n);
+
+	int sum = 0;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			sum += a[j];
+		}
+	}
+
+	printf("%d", sum);
+	free(a);
+
+}
