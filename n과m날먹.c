7aba3c687ac89c95edd618983e78a796919226b0
Erc-nard <100018037+Erc-nard@users.noreply.github.com>
2024-05-17 21:22:48 +0900

    dd

--- n과m날먹.c
+++ n과m날먹.c
@@ -10,11 +10,11 @@ void pick(int*a, int n,int* bucket,int bucketSize,int toPick){
 	}
 
     for(int i=0;i<n;i++){
-        int flag=0;
+        /* int flag=0;
         for(int j=0;j<bucketSize-toPick;j++){
             if(bucket[j]==i) flag=1;
         }
-        if(flag==1) continue;
+        if(flag==1) continue; */
         if(bucketSize!=toPick&&bucket[bucketSize-toPick-1]>i)continue;
         bucket[bucketSize-toPick]=i;
         pick(a,n,bucket,bucketSize,toPick-1);
