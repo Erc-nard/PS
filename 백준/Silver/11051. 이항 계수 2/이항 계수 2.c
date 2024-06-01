87b846d106ff4ea6c3c1acb7aa3c8be40f2b27e8
Ercnard <100018037+Erc-nard@users.noreply.github.com>
2024-06-01 20:24:38 +0900

    Update 이항 계수 2.c

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.c
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.c
@@ -10,23 +10,23 @@ long long comb(long long n, long long r,long long **m){
     if(m[n-1][r]==-1)
         m[n-1][r]=comb(n-1,r,m);
 
-    return m[n-1][r-1]%10007+m[n-1][r]%10007;
+    return m[n-1][r-1]%10007+m[n-1][r]%10007; //너무 커지지않게 중간중간 모듈러연산
 }   
 
 int main(){
     int n,k;
     scanf("%d",&n);
     scanf("%d",&k);
-    long long **m=(long long**)malloc(sizeof(long long*)*(n+1));
+    long long **m=(long long**)malloc(sizeof(long long*)*(n+1)); //sizeof(포인터)
 	
 	for(int i=0;i<n;i++)
-			m[i]=(long long*)malloc(sizeof(long long)*(n+1));
+			m[i]=(long long*)malloc(sizeof(long long)*(n+1)); //n+1조심 0부터숫자아님
 
     for(int i=0;i<n;i++)
         for(int j=0;j<n;j++)
             m[i][j]=-1;
 	
-	int C = comb(n,k,m)%10007; // 4C2
+	int C = comb(n,k,m)%10007; // 마지막 모듈러연산
 	
 	printf("%lld",C); 
 
