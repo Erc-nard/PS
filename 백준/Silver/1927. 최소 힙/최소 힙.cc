2663e3258211ea892a97ffc07219c1d27d78ef85
Ercnard <100018037+Erc-nard@users.noreply.github.com>
2025-05-12 11:36:30 +0900

    [Silver II] Title: 최소 힙, Time: 12 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); //이거하면 속도 빨라짐 
+
+    priority_queue <int, vector<int>, greater<int>> hip;
+    int n,x;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>x;
+        if(x==0){
+            if(hip.empty()){
+                cout<<"0"<<"\n";
+            }
+            else{
+                cout<<hip.top()<<"\n";
+                hip.pop();
+            }
+        }
+        else{
+            hip.push(x);
+        }
+    }
+}
